70ac27b9a98ac1b5bbae0e49b9feb8a8ac64f20c
aricw <aric.wolst@gmail.com>
2019-10-21 03:13:29 -0700

    pulling events, page reload, event profile

--- NEWEST-FRONTEND/screens/TeamProfile.js
+++ NEWEST-FRONTEND/screens/TeamProfile.js
@@ -3,34 +3,140 @@ import { Image, SafeAreaView, StyleSheet, ScrollView, Button, FlatList, View, Te
 import { createStackNavigator } from 'react-navigation-stack';
 
 class TeamProfile extends Component {
+    static navigationOptions = {
+        headerTintColor: 'white',
+        headerStyle: {
+          backgroundColor: '#ff8c00',
+        },
+        headerBackTitleStyle: {
+            color: 'white',
+            fontWeight: 'bold'
+        },
+        headerBackImageStyle: {
+            tintColor: 'white',
+        }
+    };
+
+    formatDate(data) {
+        var hour = new Date(data);
+        var min = new Date(data);
+        var day = new Date(data);
+        var month = new Date(data);
+        var ampm = "AM";
+        var year = new Date(data);
+
+        hour = hour.getUTCHours();
+        min = min.getUTCMinutes();
+        day = day.getUTCDate();
+        month = month.getUTCMonth() + 1;
+        year = year.getFullYear();
+        
+        if(hour > 12){
+            hour = hour % 12;
+            ampm = "PM"
+        }
+
+        if(min < 10){
+            min = "0"+min;
+        }
+        
+        var date = hour + ":" + min + " " + ampm + " " + month + "/" + day + "/" + year;
+        return date;
+    }
+
     render(){
         const { navigation } = this.props;
         return(
-            <View>
-                <Text>
-                    Team Name: {navigation.getParam('teamName', 'No Team Name')}
+            <ScrollView style = {{flexDirection: 'column'}}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>
+                        {navigation.getParam('eventName', 'No Event Name')}
+                    </Text>
+                </View>
+                <View style={styles.subheader}>
+                    <Text style={styles.sportText}>
+                        {navigation.getParam('sport', 'No Sport Specified')} 
+                    </Text>
+                    <Text style={styles.location}>
+                        {navigation.getParam('location', 'No Location Specified')}
+                    </Text>
+                    <Text style={styles.dateText}>
+                        {this.formatDate(navigation.getParam('date', 'No League Specified'))}
+                    </Text>
+                </View>
+                <View style={{height: 0.5, width:"80%", backgroundColor:"#ff8c00", alignSelf: "center"}}/>
+                <Text style={styles.bio}>
+                    {navigation.getParam('eventBio', ' ')}
                 </Text>
-                <Text>
-                    Bio: {navigation.getParam('teamBio', ' ')}
-                </Text>
-                <Text>
-                    Sport: {navigation.getParam('sport', 'No Sport Specified')}
-                </Text>
-                <Text>
-                    Location: {navigation.getParam('location', 'No Location Specified')}
-                </Text>
-                <Text>
-                    League: {navigation.getParam('league', 'No League Specified')}
-                </Text>
-                <Text>
-                    Skill Level: {navigation.getParam('level', 'Any Skill Level')}
-                </Text>
-                <Text>
-                    Number of Players Needed: {navigation.getParam('numPlayersNeeded', 'No Players Needed')}
-                </Text>
-            </View>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: "#ff8c00",
+        alignContent: 'center',
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    header: {
+        backgroundColor: "#ff8c00",
+        alignContent: 'center',
+        justifyContent: "center",
+        alignItems: 'center'
+      },
+    headerText: {
+        marginTop: 20,
+        marginBottom: 20,
+        fontSize: 35,
+        marginLeft: 5,
+        marginRight: 5,
+        fontWeight: 'bold',
+        color: 'white',
+        alignSelf: 'center',
+        textAlign: 'center',
+    },
+    sportText: {
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 5,
+        marginRight: 5,
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: '#ff8c00',
+        alignSelf: 'center',
+        textAlign: 'center',
+    },
+    location: {
+        marginTop: 5,
+        marginBottom: 5,
+        fontSize: 20,
+        marginLeft: 5,
+        marginRight: 5,
+        fontWeight: 'bold',
+        color: 'black',
+        alignSelf: 'center',
+        textAlign: 'center',
+    },
+    dateText: {
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 5,
+        marginRight: 5,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+        alignSelf: 'center',
+        textAlign: 'center',
+    },
+    bio: {
+        color: 'black',
+        alignSelf: 'center',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 5,
+    }
+});
 
 export default TeamProfile;
--- NEWEST-FRONTEND/screens/Teams.js
+++ NEWEST-FRONTEND/screens/Teams.js
@@ -1,65 +1,129 @@
 import React, { Component } from 'react';
-import { Image, SafeAreaView, StyleSheet, ScrollView, Button, FlatList, View, Text, ActivityIndicator, TouchableOpacity, ListView } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, ScrollView, Button, FlatList, View, Text, ActivityIndicator, TouchableOpacity, ListView, RefreshControl } from 'react-native';
 import FindTeamsHeader from '../components/FindTeamsHeader';
 import { createStackNavigator } from 'react-navigation-stack';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 class Teams extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          loading: true,
+          refreshing: true,
           dataSource: null,
+          location: null,
          };
+        this.GetData();
     }
 
-    componentDidMount(){
-        fetch("http://34.220.132.159/events/nearby?longitude=-123.252343&latitude=49.262452", {
+    static navigationOptions = {
+        headerStyle: {
+          backgroundColor: '#ff8c00',
+          color: 'white'
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          color: 'white',
+          fontSize: 30,
+        },
+    };
+
+    GetData = () =>{
+        this._getLocationAsync().then(() => {
+        fetch("http://34.220.132.159/events/nearby?longitude=" + this.state.location.coords.longitude + "&latitude=" + this.state.location.coords.latitude, {
             method: "GET",
         })
         .then(response => response.json())
         .then((responseJson)=> {
           this.setState({
-           loading: false,
+           refreshing: false,
            dataSource: responseJson
           })
-          console.log(responseJson);
-          console.log(responseJson.firstName);
         })
-        .catch(error=>console.log(error)) 
+        .catch(error=>console.log(error))
+        })
     }
+
+    onRefresh() {
+        this.setState({ dataSource: [] });
+        this.GetData();
+      }
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+          this.setState({
+            errorMessage: 'Permission to access location was denied',
+          });
+        }
+
+        const locationSettings = {
+            accuracy: Location.Accuracy.Highest,
+        }
+    
+        let location = await Location.getCurrentPositionAsync(locationSettings,{});
+        this.setState({
+            location: location 
+        });
+        console.log(location);
+    };
     
     FlatListItemSeparator = () => {
         return (
           <View style={{
              height: 0.5,
-             width:"100%",
-             backgroundColor:"rgba(0,0,0,0.5)",
+             width:"80%",
+             backgroundColor:"#ff8c00",
+             alignSelf: "center"
         }}
         />
         );
     }
 
-    goToTeam = async () => {
+    formatDate(data) {
+        var hour = new Date(data);
+        var min = new Date(data);
+        var day = new Date(data);
+        var month = new Date(data);
+        var ampm = "AM";
+        var year = new Date(data);
 
+        hour = hour.getUTCHours();
+        min = min.getUTCMinutes();
+        day = day.getUTCDate();
+        month = month.getUTCMonth() + 1;
+        year = year.getFullYear();
+        
+        if(hour > 12){
+            hour = hour % 12;
+            ampm = "PM"
+        }
+
+        if(min < 10){
+            min = "0"+min;
+        }
+        
+        var date = hour + ":" + min + " " + ampm + " " + month + "/" + day + "/" + year;
+        return date;
     }
         
     renderItem=(data)=>
     <View style = {{flexDirection: 'row'}}>
-        <View style = {{height: 100, width: '80%'}}>
+        <View style = {{height: 130, width: '80%'}}>
             <Text style={styles.sport}>{data.item.sport}</Text>
-            <Text style={styles.date}>{data.item.date}</Text>
-            <Text style={styles.location}>Location: {data.item.location.coordinates}</Text>
+            <Text style={styles.titleName}>{data.item.name}</Text>
+            <Text style={styles.date}>{this.formatDate(data.item.date)}</Text>
+            <Text style={styles.location}>{data.item.address}</Text>
         </View>
         <TouchableOpacity onPress={() => {this.props.navigation.navigate('TeamProfile', {
-            teamName: data.item.name,
-            sport: data.item.username,
-            location: data.item.address.city,
-            level: data.item.address.zipcode,
-            league: data.item.company.name,
-            numPlayersNeeded: data.item.address.geo.lat,
-            teamBio: data.item.company.catchPhrase
+            eventName: data.item.name,
+            sport: data.item.sport,
+            eventBio: data.item.description,
+            date: data.item.date,
+            location: data.item.address
         })}}>
-            <View style = {{height: 100, justifyContent: 'center', width: '20%'}}>
+            <View style = {{height: 130, justifyContent: 'center', width: '20%'}}>
                 <View style = {styles.info}>
                     <Text style = {{color: 'white', fontWeight: 'bold', fontSize: 30}}>i</Text>
                 </View>
@@ -68,7 +132,7 @@ class Teams extends Component {
     </View>
     
     render() {
-        if(this.state.loading){
+        if(this.state.refreshing){
             return( 
               <View style={styles.loader}> 
                 <ActivityIndicator size="large" color="black"/>
@@ -77,13 +141,20 @@ class Teams extends Component {
 
         return(
             <View>
-                <ScrollView>
+                <ScrollView
+                refreshControl={
+                                <RefreshControl
+                                  refreshing={this.state.refreshing}
+                                  onRefresh={this.onRefresh.bind(this)}
+                                />
+                              }>
                     <FindTeamsHeader />
                     <View style={styles.container}>
                         {<FlatList
-                            data= {this.state.dataSource}
+                            data = {this.state.dataSource}
                             ItemSeparatorComponent = {this.FlatListItemSeparator}
                             renderItem= {item=> this.renderItem(item)}
+                            keyExtractor={(item) => item._id}
                         /> }
                     </View>
                 </ScrollView>
@@ -113,28 +184,35 @@ const styles = StyleSheet.create({
     date:{
         fontSize: 20,
         marginTop: 3,
-        marginLeft: 3
+        marginLeft: 8
     },
     location:{
         fontSize: 15,
         marginBottom: 8,
         marginTop: 3,
-        marginLeft: 3
+        marginLeft: 8
     },
     sport:{
         fontWeight: "bold",
         fontSize: 25,
         marginTop: 8,
-        marginLeft: 3
+        marginLeft: 8,
+        color: "#ff8c00"
+    },
+    titleName:{
+        fontWeight: "bold",
+        fontSize: 22,
+        marginTop: 3,
+        marginLeft: 8,
+        color: "black"
     },
     info:{
-        width: 50, 
-        height: 50, 
-        borderRadius: 50/2, 
+        width: 60, 
+        height: 60, 
+        borderRadius: 60/2, 
         backgroundColor: '#ff8c00', 
-        padding: 5, 
+        padding: 8, 
         alignItems: 'center', 
         justifyContent: 'center'
     }
   });
-
