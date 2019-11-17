ad651732f76d46286102d62c2e59649b61d4b190
Rune Højlund <rune@appellante.com>
2019-11-16 19:03:24 -0800

    Add RequestToken i header on frontend.

--- NEWEST-FRONTEND/utils/RequestManager.js
+++ NEWEST-FRONTEND/utils/RequestManager.js
@@ -1,8 +1,11 @@
 import config from "../config";
-import { Alert } from "react-native";
+import { Alert, AsyncStorage } from "react-native";
 
 
 async function parseAPIResponse(response) {
+  const requestToken = response.headers.get("requesttoken");
+  if (requestToken) { await AsyncStorage.setItem("requestToken", requestToken); }
+
   if (parseInt(response.headers.get("content-length")) === 0) {
     return new Promise((resolve) => resolve(0));
   }
@@ -17,18 +20,21 @@ async function parseAPIResponse(response) {
 }
 
 export default async function backendRequest(endpoint,params,method,body) {
+  const requestToken = await AsyncStorage.getItem("requestToken");
   var url = config.apiUrl + endpoint;
   if (params && Object.entries(params).length > 0) {
     url += "?" + Object.entries(params).map((keyvalue) => keyvalue.map(encodeURIComponent).join("=")).join("&");
   }
   return new Promise((resolve) => {
     if (method === "GET") {
-      fetch(url).then((response) => {return parseAPIResponse(response);}).then((data) => resolve(data))
+      fetch(url, {
+        headers: { requesttoken: requestToken }
+      }).then((response) => {return parseAPIResponse(response);}).then((data) => resolve(data))
       .catch((err) => Alert.alert("Request Manager Promise Error", err.message));
     } else {
       fetch(url, {
         method,
-        headers: { Accept: "application/json","Content-Type": "application/json"},
+        headers: { requesttoken: requestToken, Accept: "application/json","Content-Type": "application/json"},
         body: JSON.stringify(body)
       }).then((response) => {return parseAPIResponse(response);}).then((data) => resolve(data))
       .catch((err) => Alert.alert("Request Manager Promise Error",err.message));
