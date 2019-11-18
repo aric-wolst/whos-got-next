08c773006e048346e5cd1f86bd82af312d264995
Rune Højlund <rune@appellante.com>
2019-11-18 14:06:56 -0800

    Implemented Backend unit tests.

--- whos-got-next-backend/tests/app.test.js
+++ whos-got-next-backend/tests/app.test.js
@@ -1,16 +1,12 @@
-// Logging.
-const bunyan = require("bunyan");
-const log = bunyan.createLogger({name: "whosgotnext-app-Test"});
-
-// MongoDB Connector.
 const MongoDBConnector = require("../utils/mongoDBConnector");
+const mongoose = require("mongoose");
 
-// Server Instance.
+// Start Test Server Instance.
 const app = require("../app");
-
-// Supertest.
 const supertest = require("supertest");
 const request = supertest(app);
+
+const requesttoken = "test";
 
 // Setup the mongoDB Connection before running any tests.
 beforeAll(() => {
@@ -18,35 +14,76 @@ beforeAll(() => {
     return mDBConnector.connect();
 });
 
-// Test that authentication works.
-test("Get root endpoint without request token", async (done) => {
-  log.info("Root Authentication Test");
-
-  // Sends GET Request to root endpoint.
-  const response = await request.get("/");
-  // Without a request token, we expect an error response.
-  expect(response.status).toBe(400);
-  done();
+afterAll(async () => {
+    await mongoose.connection.close();
 });
 
-// *************************************************************************
-//
-// Test with in memory mongodb database.
-// The code/test below should probably be implemented in another fashion
-// (e.g.) under the userManager.test.js by mocking dependencies, but for now
-// this illustrates that the in memory mongodb database works.
+describe("App Test", () => {
+    // Root Authentication Test Fail.
+    test("Get root endpoint without request token should fail", async (done) => {
+      request.get("/").expect(400).end(done);
+    });
 
-// MongoDB Modules.
-const mongoose = require("mongoose");
-const userSchema = require("../model/user.js");
-const User = mongoose.model("user", userSchema, "user");
+    // Root Authentication Test Success.
+    test("Get root endpoint with request token should succeed", async (done) => {
+      request.get("/").set({ requesttoken }).expect(200).end(done);
+    });
+});
 
-const userData = {authentication: {type: "facebookId", identifier: "1111111111111111", token: "AAAAAAAAAAA"}, firstName: "John", lastName: "Snow", birthday: "1993-12-24", description: "Hi there. I am from the North!", sports: [{sport: "Basketball", level: 1}, {sport: "Volleyball", level: 3}]};
+describe("User Manager Test", () => {
+    const {type, identifier, token} = {type: "facebookId", identifier: "1111111111111111", token: "AAAAAAAAAAA"};
+    const userData = {authentication: {type, identifier, token}, firstName: "John", lastName: "Snow", birthday: "1993-12-24", description: "Hi there. I am from the North!", sports: [{sport: "Basketball", level: 1}, {sport: "Volleyball", level: 3}]};
 
-test("Save user successfully", async (done) => {
-    const validUser = new User(userData);
-    const savedUser = await validUser.save();
-    // Object Id should be defined when successfully saved to MongoDB.
-    expect(savedUser._id).toBeDefined();
-    done();
+    test("Create user with invalid data should fail", async (done) => {
+        request.post("/users").set({ requesttoken, Accept: "application/json","Content-Type": "application/json" })
+        .send(JSON.stringify({firstName: "John"})).expect(400).end(done);
+    });
+
+    let userID;
+    test("Create user with data should succeed", async (done) => {
+        console.log("POST user");
+        request.post("/users").set({ requesttoken, Accept: "application/json","Content-Type": "application/json" })
+        .send(JSON.stringify(userData)).expect(200).then((response) => {
+            userID = response.body._id;
+            expect(userID).toBeDefined();
+            console.log("Did POST user");
+            done();
+        });
+    });
+
+    test("User should exist in mongoDB after creation", async (done) => {
+        console.log("GET exists user");
+        request.get("/users/exists").set({ requesttoken})
+        .query({type, identifier}).expect(200).then((response) => {
+            expect(response.body._id).toBe(userID);
+            console.log("Did GET exists user");
+            done();
+        });
+    });
+
+    test("Posting existing user should fail", async (done) => {
+        request.post("/users").set({ requesttoken, Accept: "application/json","Content-Type": "application/json" })
+        .send(JSON.stringify(userData)).expect(401).end(done);
+    });
+
+    test("Get user should succeed", async (done) => {
+        console.log("GET user");
+        request.get("/users/" + userID).set({ requesttoken })
+        .expect(200).then((response) => {
+            console.log("Did GET user");
+            expect(response.body._id).toBe(userID);
+            done();
+        });
+    });
+
+    test("Put user should succeed", async (done) => {
+        console.log("PUT user");
+        request.put("/users/" + userID).set({ requesttoken, Accept: "application/json", "Content-Type": "application/json" })
+        .send(JSON.stringify({firstName: "Jonathan"}))
+        .expect(200).then((response) => {
+            console.log("Did PUT user");
+            expect(response.body.firstName).toBe("Jonathan");
+            done();
+        });
+    });
 });
