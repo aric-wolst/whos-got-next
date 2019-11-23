eabe00d7339d6800af32dc7afb8b32e2145278b0
Rune Højlund <rune@appellante.com>
2019-11-23 14:30:43 -0800

    Updated join event function.

--- whos-got-next-backend/model/event.js
+++ whos-got-next-backend/model/event.js
@@ -26,10 +26,6 @@ const eventSchema = new mongoose.Schema({
         type: Date,
         required: [true, "An event must have a date."]
     },
-    pendingPlayerRequests: {
-        type: [String],
-        required: [true, "An event must have a (possibly empty) list of userIDs corresponding to players who wish to attend the event."]
-    },
     sport: {
         type: String,
         required: [false]
